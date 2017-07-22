d489636951daeeff90b0e427613c0f7383f0533f
jkulabuha <jkulabuha@yandex.ru>
2017-07-22 21:17:16 +0300

    task 4087

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,6 +1,15 @@
 package ru.job4j;
-
+/**
+*Class Класс для вычисления арифитических операций
+*@author akulabuhov
+*@since 22.07.2017
+*@version 1 
+*/
 public class Calculate{
+	/**
+	* Конструктор, вывод строки в консоль
+	* @param args -args.
+	*/
 	public static void main(String[] args) {
 		System.out.print("Hello world");
 	}
---
+++ chapter_001/src/main/java/ru/job4j/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author akulabuhov (mailto:jkulabuha@yandex.ru)
+* @version 1
+* @since 22.07.2017
+*/
+package ru.job4j;
